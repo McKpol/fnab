6a1c8ff57dbfbf7c106830a9893df1f4b25a3dba
McKpol <0809spinespine@gmail.com>
2024-09-14 09:33:06 +0200

    Reloadpage

--- vite.config.ts
+++ vite.config.ts
@@ -2,29 +2,26 @@ import { defineConfig } from "vite";
 
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
+import fullReload from 'vite-plugin-full-reload';
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  publicDir: "public",
+
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
   // 1. prevent vite from obscuring rust errors
   clearScreen: false,
   // 2. tauri expects a fixed port, fail if that port is not available
   server: {
-    port: 4000,
+    port: 1420,
     strictPort: true,
     host: host || false,
-    hmr: host
-      ? {
-          protocol: "ws",
-          host,
-          port: 4000,
-        }
-      : undefined,
     watch: {
       // 3. tell vite to ignore watching `src-tauri`
       ignored: ["**/src-tauri/**"],
     },
   },
+  plugins: [
+    fullReload(['src/**/*.ts', 'src/**/*.css']) // Adjust paths as needed
+  ],
 }));
